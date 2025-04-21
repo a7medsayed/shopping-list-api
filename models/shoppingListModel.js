bf17d45362f643ee62b8543b46ea68237b19b5d4
Ahmed Sayed <ahmedsayed@leannode.com>
2025-04-21 11:40:24 +0300

    validate inputs and return appropriate message

--- models/shoppingListModel.js
+++ models/shoppingListModel.js
@@ -1,19 +1,22 @@
-let shoppingList = [];
+let ShoppingList = [];
 
-const getAll = () => shoppingList;
+const getAll = () => ShoppingList;
 
 const addItem = (productId) => {
-  shoppingList.push(productId);
+  ShoppingList.push(productId);
 };
 
 const removeItem = (productId) => {
-    shoppingList = shoppingList.filter(id => id !== productId);
+    const index = ShoppingList.findIndex(item => item === productId);
+    ShoppingList =  ShoppingList.filter((_, i) => i !== index);
 };
 
-const exists = (productId) => shoppingList.some(p => p.id === productId);
+const exists = (productId) => {
+    return ShoppingList.some(p => p === productId);
+};
 
 const getTotal = () => {
-  return shoppingList.reduce((total, product) => {
+  return ShoppingList.reduce((total, product) => {
     return total + product.price;
   }, 0);
 };
